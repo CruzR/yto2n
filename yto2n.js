ff3f99292a78246b48fe7f6074076fe02eabfed8
Thomas Glamsch (CruzR) <thomas-glamsch@gmx.de>
2012-05-01 00:23:13 +0200

    Further scripting. Removed icon entry from manifest.json.

--- yto2n.js
+++ yto2n.js
@@ -1,13 +1,20 @@
-(
-var embeds = $("embed[src^='http://www.youtube.com/v/']");
-var nocookie = $("embed[src^='http://www.youtube-nocookie.com/v/']");
-var embeds_https = $("embed[src^='https://www.youtube.com/v/']");
-var nocookie_https = $("embed[src^='https://www.youtube-nocookie.com/v/']");
+encode_url = function(url) {
+	var start = new RegExp("https?://www.youtube(-nocookie)?.com/");
+	var vid = new RegExp("/[a-zA-Z0-9]{11}");
+	return start.exec(url)[0] + "embed" + vid.exec(url)[0];
+};
 
-for(var i = 0; i < embeds.length; i++) {
-	var src = embeds[i].src;
-	var dx = embeds[i].width;
-	var dy = embeds[i].height;
-	var parent = embeds[i].
-}
-)
+o2n = function() {
+	var embed = $(this).children("embed")[0];
+	var dx = embed.width;
+	var dy = embed.height;
+	var src = encode_url(embed.src);
+	return "<iframe width='" + dx + "' height='" + dy
+		+ "' src='" + src + "' frameborder='0' allowfullscreen>"
+		+ "</iframe>";
+};
+
+$("embed[src^='http://www.youtube.com/v/']:parent").replaceWith(o2n);
+$("embed[src^='http://www.youtube-nocookie.com/v/']:parent").replaceWith(o2n);
+$("embed[src^='https://www.youtube.com/v/']:parent").replaceWith(o2n);
+$("embed[src^='https://www.youtube-nocookie.com/v/']:parent").replaceWith(o2n);
