6632bb32f0e66b861ea172b9ba36207b75400190
Thomas 'CruzR' Glamsch <thomas-glamsch@gmx.net>
2012-05-01 01:47:13 +0200

    Replacing old embedding code with new embedding code works. This is v0.1.

--- yto2n.js
+++ yto2n.js
@@ -1,9 +1,18 @@
+// Setup regular expressions.
+// regex[0]: The filter rule which is used to detect old embedding style.
+// regex[1]: The first part of the new style url.
+// regex[2]: The video id of the new style url.
+regex = new Array();
+regex.push(new RegExp("https?://www.youtube(-nocookie)?.com/v/"));
+regex.push(new RegExp("https?://www.youtube(-nocookie)?.com/"));
+regex.push(new RegExp("/[\\w\\-]{11}"));
+
+// Convert old style url to new style url.
 encode_url = function(url) {
-	var start = new RegExp("https?://www.youtube(-nocookie)?.com/");
-	var vid = new RegExp("/[a-zA-Z0-9]{11}");
-	return start.exec(url)[0] + "embed" + vid.exec(url)[0];
+	return regex[1].exec(url)[0] + "embed" + regex[2].exec(url)[0];
 };
 
+// Replace old embedding style with new one.
 o2n = function() {
 	var embed = $(this).children("embed")[0];
 	var dx = embed.width;
@@ -14,7 +23,7 @@ o2n = function() {
 		+ "</iframe>";
 };
 
-$("embed[src^='http://www.youtube.com/v/']:parent").replaceWith(o2n);
-$("embed[src^='http://www.youtube-nocookie.com/v/']:parent").replaceWith(o2n);
-$("embed[src^='https://www.youtube.com/v/']:parent").replaceWith(o2n);
-$("embed[src^='https://www.youtube-nocookie.com/v/']:parent").replaceWith(o2n);
+// Find videos using old embedding style and convert them.
+$("object").filter(function(index) {
+	return ($("embed", this).length == 1) && regex[0].test($("embed", this)[0].src);
+}).replaceWith(o2n);
